9c51712edb13e045fed36fd666fc5291e7763535
SnowySlope <rivera_dana2619@yahoo.com>
2023-07-22 14:16:38 +0800

    修改public.css中类名

--- JS/优雅的方法.js
+++ JS/优雅的方法.js
@@ -1,12 +1,65 @@
 // 多种可能需要判断的时候
 let status = 6
-if(status === 0 || status === 1 || status === 2 || status === 3) {
+if (status === 0 || status === 1 || status === 2 || status === 3) {
   console.log('按钮可以点击');
 }
 // =>
 
-if([0, 1, 2, 3].includes(status)) {
+if ([0, 1, 2, 3].includes(status)) {
   console.log('按钮可以点击');
-}else{
-  console.log ("没有用啊")
+} else {
+  console.log("没有用啊")
 }
+// 如果判断一个条件满足就执行某个函数，则可以利用和运算符&&。
+let test1 = false;
+
+function callMethod() {
+  console.log('这是对的')
+}
+
+// 优化前
+if (test1) {
+  callMethod();
+}
+// 优化后
+test1 && callMethod();
+
+
+// for循环
+// 优化前
+let testData = [1, 2, 3, 4, 5, 6]
+for ( let i = 0; i < testData.length; i++ ) {
+}
+
+// 优化后
+for ( let i in testData ) {
+}
+// or
+for ( let i of testData ) {
+}
+
+// 十进制数值指数优化
+// 优化前
+for ( let i = 0; i < 10000; i++ ) {
+}
+
+// 优化后
+for ( let i = 0; i < 1e4; i++ ) {
+  console.log(i)
+}
+
+// 参数默认值
+// 优化前
+function add(test1, test2) {
+  if (test1 === undefined) {
+    test1 = 1;
+  }
+  if (test2 === undefined) {
+    test2 = 2;
+  }
+  return test1 + test2;
+}
+
+// 优化后
+add = (test1 = 1, test2 = 2) => test1 + test2;
+add();

f961b43683a39c6df0cb45a95719c3715994fcc3
真皮沙发 <l2565104767@163.com>
2023-08-21 11:41:27 +0800

    处理prettier问题

--- component-vue-app/src/views/page/home/index.vue
+++ component-vue-app/src/views/page/home/index.vue
@@ -1,12 +1,17 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+let name = "";
+console.log( name );
+</script>
 
 <template>
-	<div class="text-cyan-800 text-xs">click</div>
-	<div class="num">4646565</div>
+  <div class="btn-box bg-cyan-900 py-0.5">
+	<div class="text-center text-lime-800"></div>
+  </div>
+  <div class="text-center">4646565</div>
 </template>
 
 <style lang="scss" scoped>
 .num {
-	font-size: 16px;
+  font-size: 16px;
 }
 </style>

f06fce5b9377ae499bf17eae61108a537bc8d690
真皮沙发 <l2565104767@163.com>
2023-08-21 10:55:18 +0800

    修改名字

--- component-vue/src/api/page/user.ts
+++
@@ -1,5 +0,0 @@
-import http from "@/utils/request";
-
-export const getUserInfo = (data?: any) => {
-	return http("get", "/api/PartyGuidance/GetPartyMemberList", data);
-};
--- component-vue/src/utils/env.ts
+++
@@ -1,4 +0,0 @@
-export function getEnvValue<T = string>(key: keyof ViteEnv): T {
-	const envValue = import.meta.env[key];
-	return (envValue === "true" ? true : envValue === "false" ? false : envValue) as unknown as T;
-}
